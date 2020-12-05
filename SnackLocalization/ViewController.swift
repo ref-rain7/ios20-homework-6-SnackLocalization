6be405f4aa1437f9f23421c12ed443ad93e8f5c7
zero <zero@smail.nju.edu.cn>
2020-12-05 15:52:13 +0800

    Initial Commit

---
+++ SnackLocalization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnackLocalization
+//
+//  Created by zero on 2020/12/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
